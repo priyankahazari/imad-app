c50efd2a4adc19adb8d4606b9f3213d3aae7b1e9
priyankahazari <30730984+priyankahazari@users.noreply.github.com>
2017-08-12 19:13:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -16,6 +16,11 @@ app.get('/1.html', function(req, res) {
 app.get('/artical2', function(req, res) {
     res.sendFile(path.join(__dirname, 'ui', '2.html'));
 });
+var counter = 0;
+app.get('/counter', function (req, res) {
+    counter = counter + 1;
+    res.send(counter,toString());
+});
 
 app.get('/artical3', function(req, res) {
     res.sendFile(path.join(__dirname, 'ui', '3.html'));
