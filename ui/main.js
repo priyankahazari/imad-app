f43bc000c8256f6a2d90459370f933dddaca33f3
priyankahazari <30730984+priyankahazari@users.noreply.github.com>
2017-08-13 10:56:37 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,11 +1,6 @@
-console.log('Loaded!');
-// here is a move towards left
-var img = document.getElementById('aa');
-var marginLeft = 0;
-function moveRight () {
-    marginLeft = marginLeft + 10;
-    img.style.marginLeft = marginLeft + 'px';
-}
-img.onclick = function () {
-    var interval = setInterval(moveRight, 100);
+var button = document.getElementById('counter');
+button.onclick = function() {
+  counter = counter + 1;
+  var span = document.getElementById('count');
+  span.innerHTML = counter.toString();
 };
