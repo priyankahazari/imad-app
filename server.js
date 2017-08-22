5488cf2798fd51ef66866853d1c5e629411b53df
priyankahazari <30730984+priyankahazari@users.noreply.github.com>
2017-08-22 19:33:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,14 +1,7 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-var pool = require('pg').pool;
-var config = {
-    user: 'priyankahazari5' ,
-    database: 'priyankahazari5' ,
-    host: 'db.imad.hasura-app.io' ,
-    port: '5432' ,
-    password: process.env.DB-PASSWORD
-};
+
 var app = express();
 app.use(morgan('combined'));
 
@@ -17,18 +10,7 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-var pool = new pool(config);
-app.get('/first table-db', function (req, res) {
-    pool.query("SELECT * FROM first table", function (err, result)
-    {
-      if (err) {
-      res.status(500).send(err.toString());
-      } else {
-          res.send(JSON.stringify(result));
-      } 
-    });
-    
-});
+
 app.get('/1.html', function(req, res) {
     res.sendFile(path.join(_dirname, 'ui', '1.html'));
 });
