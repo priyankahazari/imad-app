09769f79dec97a96a0ef567e109e26063d347409
priyankahazari <30730984+priyankahazari@users.noreply.github.com>
2017-08-22 19:49:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ app.use(morgan('combined'));
 
 
 var pool = new pool(config);
-app.get('/first-db', function (req, res) {
-    pool.query("SELECT * FROM FIRST", function (err, result)
+app.get('/test-db', function (req, res) {
+    pool.query("SELECT * FROM test", function (err, result)
     {
       if (err) {
       res.status(500).send(err.toString());
