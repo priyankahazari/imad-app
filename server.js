ccd6176be72752563c7fc9506414b2fa8b561a1b
priyankahazari <30730984+priyankahazari@users.noreply.github.com>
2017-08-23 14:28:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var pool = require('pg').pool;
-var cinfig = {
+var config = {
   user: 'priyankahazari5' ,
   database: 'priyankahazari5' ,
   host: 'db.imad.hasura-app.io' ,
@@ -10,17 +10,7 @@ var cinfig = {
   password: process.env.DB_PASSWORD
 };
 var app = express();
-app.get('/test-db', function (req, res) {
-   pool.query('SELECT * FROM test', function(err, result) {
-       if (err) {
-           res.status(500).send(err.toString());
-       } else {
-           res.send(JSON,stringify(result));
-       }
-   }); 
-});
 app.use(morgan('combined'));
-var pool = new pool;
 
 var articlesS = {
     '1': {
@@ -78,9 +68,22 @@ function createTemplate (data) {
     `;
     return htmlTemplate;
 }    
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+var pool = new pool(config);
+app.get('/test-db', function (req, res) {
+   pool.query('SELECT * FROM test', function (err, result) {
+       if (err) {
+           res.status(500).send(err.toString());
+       } else {
+           res.send(JSON.stringify(result));
+       }
+   }); 
+});
+
 
 app.get('/:articleName', function (req, res) {
   var articleName  = req.params.articleName;
