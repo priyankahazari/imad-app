9842b9039011472195fb6f3f9dfed08431de6b8c
priyankahazari <30730984+priyankahazari@users.noreply.github.com>
2017-08-25 15:35:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -83,7 +83,7 @@ function hash (input, salt) {
 app.post('/create-login', function (req, res) {
    var username = req.body.username;
    var password = req.body.password;
-   var salt = crypto.RandomBytes(128).toString('hex');
+   var salt = crypto.randomBytes(128).toString('hex');
    var dbString = hash(password, salt); 
    pool.query('INSERT INTO "login" (username, password) VALUES ($1, $2)', [username, dbString], function(req, result) {
        if (err) {
