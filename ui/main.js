46974b8c7e2253ee9a4e796a531c8c068af1ad2c
priyankahazari <30730984+priyankahazari@users.noreply.github.com>
2017-08-26 13:56:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,28 @@
-var button = document.getElementById('counter');
-var counter = 0;
-button.onclick = function() {
-  counter = counter + 1;
-  var span = document.getElementById('count');
-  span.innerHTML = counter.toString();
+var submit =document.getElementById('submit_btn');
+submit onclick = function () {
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function ()
+    {
+        if (request.readystate == XMLHttpRequest.DONE) {
+            if (request.status === 200) {
+                alert('logged in successful');
+            } else if(request.status === 403) {
+                alert('username/password is incorrect');
+            } else if(request.status === 500) {
+                alert('somthng went wrong');
+            }
+        }
+            
+            
+        
+    };
+    var username = document.getElementById('username').value;
+    
+    var password = document.getElementById('password').value;
+    console.lod('username');
+    console.lod('password');
+    request.open('POST', 'http://priyankahazari5.imad.hasura-app.io/login', true);
+    request.setRequestHeader;
+    request.send(JSON.stringify({username: username, password: password}));
+    
 };
