9e6e54a048a6e479a82ec1c00c830239fe1a30fd
priyankahazari <30730984+priyankahazari@users.noreply.github.com>
2017-08-27 12:09:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -43,3 +43,12 @@ register.onclick = function () {
             
 
 };
+var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    console.log('username');
+    console.log('password');
+    request.open('POST', 'http://priyankahazari5.imad.hasura-app.io/login', true);
+    request.setRequestHeader('Content-Type', 'application/json');
+    request.send(JSON.stringify({username: username, password: password}));
+    register.value = 'registering.........';
+};
